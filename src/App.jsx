bf5f3b764eb11e1e72ca9251d4b2d3752164c0e9
Colin Hoolwerf <choolwerf@gmail.cpom>
2025-02-18 21:05:31 -0700

    stopping for the night

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./index.css";
-import { Main } from "./Components";
+import { Main } from "./Components"
 
 function App() {
   return (
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { Header, Footer } from "./index"
 
 const Main = () => {
     return (
         <div>
-            <p>HI THERE, IT'S QUIZ TIME</p>
+            <Header />
+                <div>
+                    <p>HI THERE, IT'S QUIZ TIME</p>
+                </div>
+            <Footer />
         </div>
     );
 };
