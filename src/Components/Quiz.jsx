63bbfa675023d9c6451e7205a1ccabd37441ac54
Colin Hoolwerf <choolwerf@gmail.cpom>
2025-02-20 10:11:50 -0700

    this is not a drill, we're starting!

---
+++ src/Components/Quiz.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Quiz = () => {
+    return(
+        <div>
+            <h1>Quiz Time</h1>
+            <hr />
+            <h2>What Type of Freshwater Fish are you!?</h2>
+            <hr />
+            <h3>Which one best describes you?</h3>
+            <ul>
+                <li>I'm Independent</li>
+                <li>I Prefer Large Groups</li>
+                <li>Just Please Don't Notice Me</li>
+                <li>I'm a Bit of All of That, Honestly</li>
+            </ul>
+            <button>Next</button>
+            <button>Previous</button>
+            <p>Question 1 of 5</p>
+        </div>
+    )
+}
+
+export default Quiz
--- src/Components/index.js
+++ src/Components/index.js
@@ -3,3 +3,4 @@ export { default as Footer } from "./Footer";
 export { default as Blog } from "./Blog";
 export { default as Resume } from "./Resume";
 export { default as Home } from "./Home";
+export { default as Quiz } from "./Quiz";
