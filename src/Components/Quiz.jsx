a1f5f5f2460c21fec1951e8c513b98d4afeb29af
Colin Hoolwerf <choolwerf@gmail.cpom>
2025-03-01 11:50:45 -0700

    getting closer, I can feel it

--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -1,29 +1,27 @@
 import React, { useState, useEffect } from "react";
-// import { questions } from "../db/quizQuestions.json"
 
 const Quiz = () => {
-    // const questionObject = JSON.stringify(questions)
     const [question, setQuestion] = useState([]);
-    const [currentQuestion, setCurrentQuestion] = useState(0);
-    const [finalTally, setFinalTally] = useState(0);
-    const [selectedAnswer, setSelectedAnswer] = useState(null);
+    // const [currentQuestion, setCurrentQuestion] = useState(0);
+    // const [finalTally, setFinalTally] = useState(0);
+    // const [selectedAnswer, setSelectedAnswer] = useState(null);;
 
     useEffect(() => {
-        async function fetchData() {
-          try {
-            const response = await fetch('../db/quizQuestions');
-            if (!response.ok) {
-              throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            const result = await response.json();
-            JSON.stringify(result)
-            setQuestion(result);
-          } catch (e) {
-            console.log(e);
-          }
-        }
-        fetchData();
-      }, [question]);
+        fetch('../db/quizQuestions.json', {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                // "id": "id",
+                // "text": "text",
+                // "description": "description",
+            },
+        })
+            .then(response => response.json())
+            .then((result) => {
+                setQuestion(result)
+            })
+            .catch((error) => console.log(error))
+    }, [])
 
 
     return(
@@ -31,14 +29,13 @@ const Quiz = () => {
             <h3>Which one best describes you?</h3>
             <ul>
                 <li>Question {question.text}</li>
-                {console.log(question)}
                 <li>I'm Independent</li>
                 <li>I Prefer Large Groups</li>
                 <li>Just Please Don't Notice Me</li>
                 <li>I'm a Bit of All of That, Honestly</li>
             </ul>
-            <button>Next</button>
-            <button>Previous</button>
+            <button>⬅️ Previous</button>
+            <button>Next ➡️</button>
         </div>
     )
 }
