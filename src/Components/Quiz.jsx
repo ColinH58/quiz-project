9fdc39feae14401e5c500e634f82582adbe1fee1
Colin Hoolwerf <choolwerf@gmail.cpom>
2025-03-01 11:09:55 -0700

    trying stuff

--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -1,22 +1,37 @@
 import React, { useState, useEffect } from "react";
+// import { questions } from "../db/quizQuestions.json"
 
 const Quiz = () => {
+    // const questionObject = JSON.stringify(questions)
     const [question, setQuestion] = useState([]);
     const [currentQuestion, setCurrentQuestion] = useState(0);
     const [finalTally, setFinalTally] = useState(0);
     const [selectedAnswer, setSelectedAnswer] = useState(null);
 
-    useEffect(() => { 
-        const quizQuestions = [
-            { text: 'Which Option Best Describes You?', options: ['A', 'B', 'C', 'D'] },
-        ];
-        setQuestion(quizQuestions);
-    }, []);
+    useEffect(() => {
+        async function fetchData() {
+          try {
+            const response = await fetch('../db/quizQuestions');
+            if (!response.ok) {
+              throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const result = await response.json();
+            JSON.stringify(result)
+            setQuestion(result);
+          } catch (e) {
+            console.log(e);
+          }
+        }
+        fetchData();
+      }, [question]);
+
 
     return(
         <div>
             <h3>Which one best describes you?</h3>
             <ul>
+                <li>Question {question.text}</li>
+                {console.log(question)}
                 <li>I'm Independent</li>
                 <li>I Prefer Large Groups</li>
                 <li>Just Please Don't Notice Me</li>
